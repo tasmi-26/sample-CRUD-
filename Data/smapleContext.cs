528642abf3ef22ccb6e6865935533bc5d7c75419
tazul <tazul@AUTO4MS8BDV3BRB>
2024-10-01 12:01:22 +0530

    Add project files.

---
+++ Data/smapleContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using smaple.Models;
+
+namespace smaple.Data
+{
+    public class smapleContext : DbContext
+    {
+        public smapleContext (DbContextOptions<smapleContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<smaple.Models.Student> Student { get; set; } = default!;
+    }
+}
---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace smaple.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
